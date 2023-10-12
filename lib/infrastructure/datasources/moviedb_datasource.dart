38cc23eed38d03461636249eac4d04812aef7a53
Sebastian Godirio <sebastianlgodirio@gmail.com>
2023-10-12 11:46:01 -0300

    add cinemapedia fisrt steps

---
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -0,0 +1,24 @@
+import 'package:cinemapedia/config/constants/environment.dart';
+import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/mappers/movie_mapper.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/moviedb_response.dart';
+import 'package:dio/dio.dart';
+
+class MovieDbDataSource extends MoviesDatasource {
+  final dio = Dio(BaseOptions(
+      baseUrl: 'https://api.themoviedb.org/3',
+      queryParameters: {'api_key': Environment.apiKey}));
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) async {
+    final response = await dio.get('/movie/now_playing');
+    final movieDbResponse = MovieDbResponse.fromJson(response.data);
+    final List<Movie> movies = movieDbResponse.results
+        .where((moviedb) => moviedb.posterPath != 'no-poster')
+        .map((e) => MovieMapper.movieDBToEntity(e))
+        .toList();
+
+    return movies;
+  }
+}
